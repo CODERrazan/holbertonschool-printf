3282eecde322935a14c68e1d8d69404ba4e0814b
razan <razan_8801@icloud.com>
2025-03-24 23:39:58 +0000

    last update

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_char - Prints a single character.
 * @args: The argument list containing the character.
@@ -10,6 +11,7 @@ int print_char(va_list args)
 char c = va_arg(args, int);
 return (write(1, &c, 1));
 }
+
 /**
 * print_string - Prints a string.
 * @args: The argument list containing the string.
@@ -29,6 +31,7 @@ count += write(1, str++, 1);
 
 return (count);
 }
+
 /**
 * print_percent - Prints a literal percent sign.
 *
@@ -38,6 +41,7 @@ int print_percent(void)
 {
 return (write(1, "%", 1));
 }
+
 /**
 * print_number_recursive - Recursively prints the digits of a number.
 * @n: The unsigned number to print.
@@ -47,12 +51,15 @@ return (write(1, "%", 1));
 int print_number_recursive(unsigned int n)
 {
 int count = 0;
+
 if (n / 10)
 count += print_number_recursive(n / 10);
+
 count += write(1, &"0123456789"[n % 10], 1);
 
 return (count);
 }
+
 /**
 * print_number - Prints an integer.
 * @args: The argument list containing the integer.
@@ -64,6 +71,7 @@ int print_number(va_list args)
 int n = va_arg(args, int);
 unsigned int num;
 int count = 0;
+
 if (n < 0) /* Handle negative numbers */
 {
 count += write(1, "-", 1);
@@ -77,6 +85,7 @@ num = n;
 count += print_number_recursive(num);
 return (count);
 }
+
 /**
 * _printf - Produces output according to a format.
 * @format: A character string containing directives.
@@ -88,9 +97,12 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
+
 if (!format)
 return (-1);
+
 va_start(args, format);
+
 while (*format)
 {
 if (*format == '%') /* Format specifier detected */
@@ -98,6 +110,7 @@ if (*format == '%') /* Format specifier detected */
 format++;
 if (!*format)
 return (-1);
+
 if (*format == 'c')
 count += print_char(args);
 else if (*format == 's')
@@ -118,6 +131,7 @@ count += write(1, format, 1); /* Regular character */
 }
 format++;
 }
+
 va_end(args);
 return (count);
 }
