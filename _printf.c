886a904190ac2dcd44b6fdd5c12f6beee38771ae
razan <razan_8801@icloud.com>
2025-03-24 23:49:12 +0000

    last update2

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_char - Prints a single character.
 * @args: The argument list containing the character.
@@ -11,7 +10,6 @@ int print_char(va_list args)
 char c = va_arg(args, int);
 return (write(1, &c, 1));
 }
-
 /**
 * print_string - Prints a string.
 * @args: The argument list containing the string.
@@ -22,16 +20,12 @@ int print_string(va_list args)
 {
 char *str = va_arg(args, char *);
 int count = 0;
-
 if (!str)
 str = "(null)";
-
 while (*str)
 count += write(1, str++, 1);
-
 return (count);
 }
-
 /**
 * print_percent - Prints a literal percent sign.
 *
@@ -41,51 +35,6 @@ int print_percent(void)
 {
 return (write(1, "%", 1));
 }
-
-/**
-* print_number_recursive - Recursively prints the digits of a number.
-* @n: The unsigned number to print.
-*
-* Return: Number of characters printed.
-*/
-int print_number_recursive(unsigned int n)
-{
-int count = 0;
-
-if (n / 10)
-count += print_number_recursive(n / 10);
-
-count += write(1, &"0123456789"[n % 10], 1);
-
-return (count);
-}
-
-/**
-* print_number - Prints an integer.
-* @args: The argument list containing the integer.
-*
-* Return: Number of characters printed.
-*/
-int print_number(va_list args)
-{
-int n = va_arg(args, int);
-unsigned int num;
-int count = 0;
-
-if (n < 0) /* Handle negative numbers */
-{
-count += write(1, "-", 1);
-num = -n;
-}
-else
-{
-num = n;
-}
-
-count += print_number_recursive(num);
-return (count);
-}
-
 /**
 * _printf - Produces output according to a format.
 * @format: A character string containing directives.
@@ -93,19 +42,17 @@ return (count);
 * Return: Number of characters printed (excluding null byte),
 *         or -1 if an error occurs.
 */
+
 int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
-
 if (!format)
 return (-1);
-
 va_start(args, format);
-
 while (*format)
 {
-if (*format == '%') /* Format specifier detected */
+if (*format == '%')
 {
 format++;
 if (!*format)
@@ -117,8 +64,6 @@ else if (*format == 's')
 count += print_string(args);
 else if (*format == '%')
 count += print_percent();
-else if (*format == 'd' || *format == 'i')
-count += print_number(args);
 else
 {
 count += write(1, "%", 1);
@@ -127,11 +72,10 @@ count += write(1, format, 1);
 }
 else
 {
-count += write(1, format, 1); /* Regular character */
+count += write(1, format, 1);
 }
 format++;
 }
-
 va_end(args);
 return (count);
 }
