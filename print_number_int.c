3e737c4d79d5013be48bb815ef14b2057fa859fb
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-07 08:38:49 +0000

    Add print_number_with_flags and print_number_int support

---
+++ print_number_int.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number_int - prints an integer to stdout
+ * @n: number to print
+ * Return: number of characters printed
+ */
+int print_number_int(int n)
+{
+    int count = 0;
+
+    if (n < 0)
+    {
+        count += write(1, "-", 1);
+        n = -n;
+    }
+
+    if (n / 10)
+        count += print_number_int(n / 10);
+
+    count += write(1, &((char[]){"0123456789"}[n % 10]), 1);
+    return (count);
+}
--- test_binary.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-    _printf("Binary of 5: %b\n", 5);
-    _printf("Binary of 98: %b\n", 98);
-    _printf("Binary of 0: %b\n", 0);
-    return (0);
-}
